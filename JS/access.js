27bf849161d89b64387789bb831aace1d244e247
diogotavc <diogotavc@protonmail.com>
2024-05-22 20:33:50 +0100

    Improve room temperature adjustment system

--- JS/access.js
+++ JS/access.js
@@ -1,5 +1,9 @@
 function changeTemp(increment) {
     var temp = parseInt(document.getElementById('temp').innerHTML);
 
-    document.getElementById('temp').innerHTML = temp + parseInt(increment) + 'º';
+    if(increment > 0 && temp < 30) {
+        document.getElementById('temp').innerHTML = temp + parseInt(increment) + 'º';
+    } else if(increment < 0 && temp > 15) {
+        document.getElementById('temp').innerHTML = temp + parseInt(increment) + 'º';
+    }
 }
