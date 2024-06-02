dd86fbb65266588d9d5d0429d1ca2eacb7f44908
JoaoBastos023 <joaop.bastos@ua.pt>
2024-06-02 19:17:55 +0100

    Reservation system

--- JS/reservation.js
+++ JS/reservation.js
@@ -1,16 +1,17 @@
 function reserve(id) {
     var resource = JSON.parse(localStorage.getItem(id));
 
-    if (resource.available == 0) {
-        alert('Recurso indisponível!')
-    }
-    else {
+    if(resource == null || resource.available == 1) {
         localStorage.setItem('toReserve', JSON.stringify(id));
         if (id > 100) 
             window.location.href = '../HTML/reserve_room.html';
         else 
             window.location.href = '../HTML/reserve_printer.html';
-        }
+    }
+
+    else {
+        alert('Recurso indisponível!')
+    }
 }
 
 function validateEmail(email) {
@@ -41,11 +42,49 @@ function makeReservation() {
 
             // add user to the resource
             window.localStorage.setItem(resource, JSON.stringify({'user' : email.value, 'available' : 0}));
-            window.location.href = '../HTML/temperature.html';
+            
+            if(resource > 100)
+                window.location.href = '../HTML/temperature.html';
+            else
+                window.location.href = '../HTML/printer.html';
         }
         else 
             alert('Email inválido!');
     }
     else 
         alert('Insira o email!');
+}
+
+function getResourceName() {
+    var resource = JSON.parse(localStorage.getItem('toReserve'));
+    console.log(resource);
+
+    if(resource > 100) 
+        return "Sala - " + resource;
+
+    else 
+        return "Impressora - " + resource;
+}
+
+function endReservation() {
+    // Get resource in question
+    var resource = JSON.parse(localStorage.getItem('toReserve'));
+
+    // Clear resource
+    localStorage.setItem(resource, JSON.stringify({"user" : "", "available" : 1}));
+
+    // Remove resource from user
+    var resources = JSON.parse(localStorage.getItem(localStorage.getItem('loggedUser')));
+
+    for(var i = 0; i < resources.length; i++) {
+        if ( resources[i] == resource) {
+            resources.splice(i, 1);
+        }
+    }
+
+    //Update localstorage
+    localStorage.setItem(localStorage.getItem('loggedUser'), JSON.stringify(resources));
+
+    //Redirect to initial page
+    window.location.href = '../HTML/resource_access.html';
 }
--- JS/resources.js
+++ JS/resources.js
@@ -5,32 +5,32 @@ var room150 = JSON.parse(localStorage.getItem(150));
 var printer1 = JSON.parse(localStorage.getItem(1));
 var printer2 = JSON.parse(localStorage.getItem(2));
 
-if (room120.available == 1 || room120 == null)
+if (room120 == null || room120.available == 1)
     document.getElementById('120').style.background = '#33FF00';
 else
     document.getElementById('120').style.background = 'red';
 
-if (room130.available == 1 || room130 == null)
+if (room130 == null || room130.available == 1)
     document.getElementById('130').style.background = '#33FF00';
 else
     document.getElementById('130').style.background = 'red';
 
-if (room140.available == 1 || room140 == null)
+if (room140 == null || room140.available == 1)
     document.getElementById('140').style.background = '#33FF00';
 else
     document.getElementById('140').style.background = 'red';
 
-if (room150.available == 1 || room150 == null)
+if (room150 == null || room150.available == 1)
     document.getElementById('150').style.background = '#33FF00';
 else
     document.getElementById('150').style.background = 'red';
 
-if (printer1.available == 1 || printer1 == null)
+if (printer1 == null || printer1.available == 1)
     document.getElementById('1').style.background = '#33FF00';
 else
     document.getElementById('1').style.background = 'red';
 
-if (printer2.available == 1 || printer2 == null)
+if (printer2 == null || printer2.available == 1)
     document.getElementById('2').style.background = '#33FF00';
 else
     document.getElementById('2').style.background = 'red';
