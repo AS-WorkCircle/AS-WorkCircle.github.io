c2aa3b80d7adacb40000def6f59388fe5f871b99
diogotavc <diogotavc@protonmail.com>
2024-06-05 14:50:06 +0100

    Verificar área de texto

--- JS/helpdesk.js
+++ JS/helpdesk.js
@@ -4,15 +4,20 @@ function validateEmail(email) {
     return re.test(email);
 }
 
-// Make a reservation
+// Submit a Ticket
 function submitTicket() {
     var email = document.getElementById('email');
+    var ticket = document.getElementById('ticket');
 
     if(email) {
         var email = email.value;
         if(validateEmail(email)) {
-            document.getElementById('ticket').value = "";
-            window.location.reload();
+            if (ticket.value == '' || ticket.value == ticket.placeholder) {
+                alert('Descreva o seu problema!');
+            } else {
+                ticket.value = "";
+                window.location.reload();
+            }
         } else alert('Email inválido!');
     } else alert('Insira o email!');
 }
